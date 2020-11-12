803a5be6ac544b9ea7d007647d2f0c2e43d07f1e
RomantsovS <romantsov_s@rusklimat.ru>
2020-11-12 17:01:08 +0300

    echoAndError

--- lib/DBManage.groovy
+++ lib/DBManage.groovy
@@ -179,8 +179,11 @@ def createDB(platform, server1c, serversql, base, cfdt, isras) {
 
     def command = "oscript one_script_tools/dbcreator.os ${platformLine} -server1c ${server1c} -serversql ${serversql} -base ${base} ${cfdtpath}";
     returnCode = commonMethods.cmdReturnStatusCode(command)
+    
+    echo returnCode
+    
     if (returnCode != 0) {
-        commonMethods.raiseError("Возникла ошибка при создании базы ${base} в кластере ${serversql}")
+        commonMethods.echoAndError("Возникла ошибка при создании базы ${base} в кластере ${serversql}")
     }
 }
 
