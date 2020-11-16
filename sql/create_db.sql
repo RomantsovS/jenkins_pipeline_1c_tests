d1eecd55186ab17c0a0fcaa9e4309de727d117fc
RomantsovS <romantsov_s@rusklimat.ru>
2020-11-16 12:38:10 +0300

    PRINT N'DB is not exists.';

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -3,7 +3,10 @@ DECLARE @mydb nvarchar(50);
 set @mydb = '$(restoreddb)';
 if db_id(@mydb) is null
 begin
-declare @sqltext nvarchar(50);
-set @sqltext = 'create database '+@mydb;
-exec(@sqltext)
+	PRINT N'DB is not exists.';
+	declare @sqltext nvarchar(50);
+	set @sqltext = 'create database '+@mydb;
+	exec(@sqltext)
+else
+	PRINT N'DB already exists.';
 end
