c702a237f137f327cab122ff45f463c232d6678d
RomantsovS <romantsov_s@rusklimat.ru>
2020-11-16 14:09:17 +0300

    'MD '+ @folderPath

--- sql/delete_files.sql
+++ sql/delete_files.sql
@@ -9,4 +9,8 @@ set @folderPath = '$(folderPath)';
 if @folderPath == N''
 	RAISERROR ('Error, incorrect path', 11, 1)
 
+declare @cmdpath nvarchar(60)
+set @cmdpath = 'MD '+ @folderPath
+exec master.dbo.xp_cmdshell @cmdpath
+
 EXECUTE master.dbo.xp_delete_file @typeFile, @folderPath, @fileExtension, @subFolder;
