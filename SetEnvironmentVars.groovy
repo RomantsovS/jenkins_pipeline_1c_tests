db21e5637bf31239d2709fe773c4c395f7aa4ab4
RomantsovS <romantsov_s@rusklimat.ru>
2020-11-25 16:29:20 +0300

    env.TIMEOUT_FOR_CHECKOUT_STAGE = "60"

--- SetEnvironmentVars.groovy
+++ SetEnvironmentVars.groovy
@@ -22,6 +22,9 @@ env.git_repo_branch = "master"
 
 env.PROPERTIES_CATALOG = "./tests"
 
+env.TIMEOUT_FOR_INIT_STAGE = "3"
+env.TIMEOUT_FOR_CHECKOUT_STAGE = "60"
+
 //параметры для переопределения в одноименном файле в репозитории проекта в каталоге env.PROPERTIES_CATALOG/
 env.SERVER_SQL = "DB01"
 
@@ -49,8 +52,6 @@ env.ADMIN_1C_PWD = ""
 env.PATH_TO_1C = "\\\\rusklimat.ru\\app\\1Cv8ADM\\8.3.12.1685_CR\\bin\\1cv8c.exe" //если env.USE_VANESSA_RUNNER = "false"
 env.PATH_TO_VANESSA_AUTOMATION = "\\\\dev1c\\vanessa-automation\\vanessa-automation.epf"
 
-env.TIMEOUT_FOR_INIT_STAGE = "3"
-env.TIMEOUT_FOR_CHECKOUT_STAGE = "10"
 env.TIMEOUT_FOR_DELETE_TEST_DB_STAGE = "4"
 env.TIMEOUT_FOR_SQL_BACKUP_TEMPLATE_DB_STAGE = "5"
 env.TIMEOUT_FOR_SQL_RESTORE_TEMPLATE_DB_STAGE = "6"
